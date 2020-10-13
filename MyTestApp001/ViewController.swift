5fa92e1b2061abdbc37f9db2950a3ca0e6ea2971
smatsuo415 <shu55555.m@gmail.com>
2020-10-14 07:36:03 +0900

    second commit

--- MyTestApp001/ViewController.swift
+++ MyTestApp001/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func log() {
+        print("テスト")
+    }
 
 
 }
