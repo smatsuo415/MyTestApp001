186c1c83748b83947f0019bb000cf8cf0bf48f2f
smatsuo415 <shu55555.m@gmail.com>
2020-10-14 07:08:34 +0900

    first commit

---
+++ MyTestApp001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTestApp001
+//
+//  Created by 松尾周吾 on 2020/08/24.
+//  Copyright © 2020 smatsuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var changeLabel: UILabel!
+    @IBAction func changeLabel(_ sender: Any) {
+        changeLabel.text = "Second Label"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
